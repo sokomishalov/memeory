0d707406cce2d650fad1aaae6b159542d9f1b713
sokomishalov <Sokolov.M.Andre@sberbank.ru>
2019-07-29 19:12:42 +0300

    flutter: android fixes

--- mobile-app/memeory/lib/main_local.dart
+++ mobile-app/memeory/lib/main_local.dart
@@ -1,10 +1,12 @@
-import 'app.dart';
 import 'package:memeory/util/env.dart';
+import 'package:memeory/util/os.dart';
+
+import 'app.dart';
 
 void main() {
   BuildEnvironment.init(
     flavor: BuildFlavor.local,
-    backendUrl: "http://localhost:8080",
+    backendUrl: isAndroid() ? "http://10.0.2.2:8080" : "http://localhost:8080",
   );
 
   runMemeory();
--- mobile-app/memeory/lib/util/os.dart
+++ mobile-app/memeory/lib/util/os.dart
@@ -1,5 +1,9 @@
 import 'dart:io' show Platform;
 
+bool isAndroid() => Platform.isAndroid;
+
+bool isIos() => Platform.isIOS;
+
 bool isMobile() => Platform.isIOS || Platform.isAndroid;
 
 bool isDesktop() => !isMobile();
