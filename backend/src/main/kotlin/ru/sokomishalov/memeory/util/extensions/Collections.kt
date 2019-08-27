8f718138c15ef3784ec2eefe9ade3ddc75718cea
sokomishalov <vtb218497@corp.dev.vtb>
2019-08-27 16:05:53 +0300

    some refactoring

--- backend/src/main/kotlin/ru/sokomishalov/memeory/util/extensions/Collections.kt
+++ backend/src/main/kotlin/ru/sokomishalov/memeory/util/extensions/Collections.kt
@@ -2,6 +2,7 @@ package ru.sokomishalov.memeory.util.extensions
 
 import kotlinx.coroutines.Dispatchers.Unconfined
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withContext
 
 
@@ -10,16 +11,16 @@ import kotlinx.coroutines.withContext
  */
 
 suspend inline fun <T> Iterable<T>.aForEach(noinline block: suspend (T) -> Unit) = withContext(Unconfined) {
-    map { async { block(it) } }.forEach { it.await() }
+    map { async { block(it) } }.awaitAll()
 }
 
 suspend inline fun <T, R> Iterable<T>.aMap(noinline transform: suspend (T) -> R): List<R> = withContext(Unconfined) {
-    map { async { transform(it) } }.map { it.await() }
+    map { async { transform(it) } }.awaitAll()
 }
 
 suspend inline fun <T> Iterable<T>.aFilter(crossinline predicate: suspend (T) -> Boolean): List<T> = withContext(Unconfined) {
     val destination = ArrayList<T>()
-    map { async { if (predicate(it)) destination.add(it) } }.forEach { it.await() }
+    map { async { if (predicate(it)) destination.add(it) } }.awaitAll()
     destination
 }
 
