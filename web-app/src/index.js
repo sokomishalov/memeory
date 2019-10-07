6442004ab25c3eb59f6dac26ade0696e0433528e
sokomishalov <vtb218497@corp.dev.vtb>
2019-10-07 14:18:59 +0300

    frontend: added fonts

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './app/common/styles/index.css';
 import './app/common/styles/boilerplates.css'
 import './app/common/styles/antd.css';
+import './app/common/styles/antd-mobile.css';
 import * as sw from './app/sw/sw';
 import {App} from "./app/App";
 
