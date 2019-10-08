126f382bc76ede0febc2d9a17aba424ea8156883
sokomishalov <vtb218497@corp.dev.vtb>
2019-10-08 19:17:45 +0300

    frontend: auth started

--- web-app/src/app/auth/LoginModal.js
+++ web-app/src/app/auth/LoginModal.js
@@ -1,5 +1,8 @@
 import React, {useState} from "react";
 import {Modal} from 'antd';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+import {FACEBOOK_APP_ID} from "../../util/env/env";
 
 export const LoginModal = ({button}) => {
 
@@ -7,6 +10,9 @@ export const LoginModal = ({button}) => {
 
     const openModal = () => setVisible(true);
     const closeModal = () => setVisible(false);
+
+    const responseGoogle = (response) => console.log(response);
+    const responseFacebook = (response) => console.log(response);
 
     return (
         <div>
@@ -17,7 +23,23 @@ export const LoginModal = ({button}) => {
                    visible={visible}
                    onCancel={closeModal}
                    footer={null}>
-                TODO
+
+                <div>
+                    <GoogleLogin clientId="142446820744-2ev2p083grumiv089k4dk6ai6q5d7h41.apps.googleusercontent.com"
+                                 buttonText="Авторизуйтесь через Google"
+                                 theme="dark"
+                                 scope="https://www.googleapis.com/auth/userinfo.profile"
+                                 onSuccess={responseGoogle}
+                                 onFailure={responseGoogle}
+                    />
+                </div>
+                <div className="mt-20">
+                    <FacebookLogin appId={FACEBOOK_APP_ID}
+                                   textButton="Авторизуйтесь через Facebook"
+                                   size="small"
+                                   fields="name,first_name,last_name,email"
+                                   callback={responseFacebook}/>
+                </div>
             </Modal>
         </div>
     );
---
+++ web-app/src/util/env/env.js
@@ -0,0 +1,2 @@
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+export const FACEBOOK_APP_ID = process.env.REACT_APP_FACEBOOK_APP_ID;
