826d632fe3d0419ee5d9ad3de3626effd0002a79
sokomishalov <vtb218497@corp.dev.vtb>
2019-10-10 14:09:04 +0300

    frontend: lightweight video player

--- web-app/src/app/memes/container/video/VideoAttachment.js
+++ web-app/src/app/memes/container/video/VideoAttachment.js
@@ -4,5 +4,15 @@ import ReactPlayer from 'react-player'
 export const VideoAttachment = ({attachment}) => (
     <ReactPlayer url={attachment["url"]}
                  width="100%"
-                 height="100%"/>
+                 height="100%"
+                 light={true}
+                 config={{
+                     youtube: {
+                         playerVars: {
+                             showinfo: 0,
+                             playsinline: 1,
+                             origin: process.env.PUBLIC_URL
+                         }
+                     }
+                 }}/>
 );
