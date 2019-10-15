d1c0a9f39e5e675b10d3c5a8a757e00e71e10278
sokomishalov <vtb218497@corp.dev.vtb>
2019-10-15 17:40:55 +0300

    backend: minor improvements

--- backend/src/main/kotlin/ru/sokomishalov/memeory/service/db/mongo/config/MongoConfig.kt
+++ backend/src/main/kotlin/ru/sokomishalov/memeory/service/db/mongo/config/MongoConfig.kt
@@ -1,6 +1,9 @@
 package ru.sokomishalov.memeory.service.db.mongo.config
 
 import com.mongodb.MongoClientOptions
+import com.mongodb.reactivestreams.client.MongoClient
+import org.springframework.boot.autoconfigure.mongo.MongoProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -9,6 +12,7 @@ import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import ru.sokomishalov.commons.spring.locks.cluster.mongo.MongoReactiveLockProvider
 import ru.sokomishalov.memeory.util.consts.MONGO_KEY_DOT_REPLACEMENT
 import java.time.Duration.ofSeconds
 
@@ -18,7 +22,13 @@ import java.time.Duration.ofSeconds
  */
 @Configuration
 @EnableReactiveMongoRepositories("ru.sokomishalov.memeory.service.db.mongo.repository")
+@EnableConfigurationProperties(MongoProperties::class)
 class MongoConfig {
+
+    @Bean
+    fun mongoReactiveLockProvider(client: MongoClient, props: MongoProperties): MongoReactiveLockProvider {
+        return MongoReactiveLockProvider(client, props)
+    }
 
     @Bean
     @Primary
