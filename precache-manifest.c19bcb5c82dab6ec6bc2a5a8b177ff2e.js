d91c01a34058c7fc69d84a818986a26ab9522872
sokomishalov <sokomishalov@mail.ru>
2019-10-18 00:20:07 +0300

    Updates

---
+++ precache-manifest.c19bcb5c82dab6ec6bc2a5a8b177ff2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd28f4a000e19be5afe9a4160aa4528",
+    "url": "/memeory/index.html"
+  },
+  {
+    "revision": "d18b12bb96b3a68d851f",
+    "url": "/memeory/static/css/2.f763cd6c.chunk.css"
+  },
+  {
+    "revision": "cd5bc6ece266f43c1bce",
+    "url": "/memeory/static/css/main.7e7b53ea.chunk.css"
+  },
+  {
+    "revision": "d18b12bb96b3a68d851f",
+    "url": "/memeory/static/js/2.98bd082c.chunk.js"
+  },
+  {
+    "revision": "fca4c6e17cd38c4bcb56",
+    "url": "/memeory/static/js/3.2b86f2e2.chunk.js"
+  },
+  {
+    "revision": "cd5bc6ece266f43c1bce",
+    "url": "/memeory/static/js/main.e98c3b1f.chunk.js"
+  },
+  {
+    "revision": "16ea7016b4fc88dca189",
+    "url": "/memeory/static/js/runtime-main.475524db.js"
+  },
+  {
+    "revision": "d22af8f5e4faa2864df66390ed45d1ec",
+    "url": "/memeory/static/media/404.d22af8f5.png"
+  }
+]);
