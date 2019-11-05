07f5a090a34918ef367aa17295471979a7dfc081
sokomishalov <sokomishalov@mail.ru>
2019-11-06 01:11:54 +0300

    socials further progress

--- mobile-app/memeory/lib/cache/repository/socials_repo.dart
+++ mobile-app/memeory/lib/cache/repository/socials_repo.dart
@@ -6,10 +6,10 @@ import 'package:memeory/util/consts/consts.dart';
 import '../storage.dart';
 
 Future<bool> isAuthorized() async {
-  return (await getProfilesMap()).isNotEmpty;
+  return (await getSocialsMap()).isNotEmpty;
 }
 
-Future<Map> getProfilesMap() async {
+Future<Map> getSocialsMap() async {
   var resultMap = {};
   [GOOGLE_PROVIDER, FACEBOOK_PROVIDER].forEach((p) async {
     var account = await getSocialsAccount(p);
@@ -20,21 +20,12 @@ Future<Map> getProfilesMap() async {
   return resultMap;
 }
 
-putProfilesMap(socialsMap) async {
-  [GOOGLE_PROVIDER, FACEBOOK_PROVIDER].forEach((p) async {
-    var map = socialsMap[p] ?? {};
-    if (map.isNotEmpty) {
-      await setSocialsAccount(p, ProviderAuth.fromJson(map));
-    }
-  });
-}
-
 Future<ProviderAuth> getSocialsAccount(String provider) async {
   var profile = await get(provider);
   var decodedProfile = profile != null ? json.decode(profile) : <String, dynamic>{};
   return ProviderAuth.fromJson(decodedProfile);
 }
 
-setSocialsAccount(String provider, ProviderAuth profile) async {
+putSocialsAccount(String provider, ProviderAuth profile) async {
   await put(provider, json.encode(profile.toJson()));
 }
