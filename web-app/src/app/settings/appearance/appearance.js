a126064bdbeae13342e1a2c1c182bb47b9579b11
sokomishalov <sokomishalov@mail.ru>
2019-12-19 21:44:31 +0300

    appearance further progress

--- web-app/src/app/settings/appearance/appearance.js
+++ web-app/src/app/settings/appearance/appearance.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {withT} from "../../../util/locales/i18n";
+import { withT } from "../../../util/locales/i18n";
+import { Popover, Switch } from "antd";
 
-const Appearance = () => (
-    <>
-    </>
+const Appearance = ({t}) => (
+    <div>
+        <div className="settings-content-section">
+            <div className="caption">{ t("dark.theme") }</div>
+            <Popover content={ t("light.theme.not.implemented") } trigger="hover">
+                <Switch disabled={ true } defaultChecked/>
+            </Popover>
+        </div>
+    </div>
 );
 
 export default withT(Appearance);
