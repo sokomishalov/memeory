36988c8c65ff94cc625b2d0c3bf61621ae157fdd
sokomishalov <sokomishalov@mail.ru>
2020-01-02 02:03:52 +0300

    flutter redux further progress

--- mobile-app/memeory/lib/pages/channels/channels.dart
+++ mobile-app/memeory/lib/pages/channels/channels.dart
@@ -1,143 +1,131 @@
 import 'package:flutter/material.dart';
-import 'package:memeory/api/channels.dart';
-import 'package:memeory/api/topics.dart';
-import 'package:memeory/components/containers/future_builder.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:memeory/components/images/channel_logo.dart';
 import 'package:memeory/components/images/provider_logo.dart';
 import 'package:memeory/model/channel.dart';
 import 'package:memeory/model/topic.dart';
 import 'package:memeory/pages/memes/memes_screen_args.dart';
+import 'package:memeory/store/state/app_state.dart';
 import 'package:memeory/util/routes/routes.dart';
 import 'package:memeory/util/strings/strings.dart';
+import 'package:redux/redux.dart';
 
-class ChannelPreferences extends StatefulWidget {
-  @override
-  _ChannelPreferencesState createState() => _ChannelPreferencesState();
-}
-
-class _ChannelPreferencesState extends State<ChannelPreferences> {
+class ChannelPreferences extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        children: [
-          FutureWidget(
-            future: Future.wait([
-              fetchChannels(),
-              fetchTopics(),
-            ]),
-            render: (data) {
-              List<Channel> channels = data[0];
-              List<Topic> topics = data[1];
+      child: StoreConnector<AppState, List<Channel>>(
+        converter: (Store<AppState> store) => store.state.channels,
+        builder: (BuildContext context, List<Channel> channels) {
+          return GridView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            shrinkWrap: true,
+            itemCount: channels.length,
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 180,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              var item = channels[index];
+              var id = item.id;
 
-              return Expanded(
-                child: GridView.builder(
-                  padding: EdgeInsets.symmetric(horizontal: 5),
-                  shrinkWrap: true,
-                  itemCount: channels.length,
-                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 180,
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    var item = channels[index];
-                    var id = item.id;
-
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                          context,
-                          ROUTES.MEMES.route,
-                          arguments: MemesScreenArgs(
+              return GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacementNamed(
+                    context,
+                    ROUTES.MEMES.route,
+                    arguments: MemesScreenArgs(
+                      channelId: id,
+                    ),
+                  );
+                },
+                child: GridTile(
+                  child: Card(
+                    color: CardTheme.of(context).color,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          child: ChannelLogo(
                             channelId: id,
                           ),
-                        );
-                      },
-                      child: GridTile(
-                        child: Card(
-                          color: CardTheme.of(context).color,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 7),
+                          child: Text(
+                            item.name ?? id,
+                            softWrap: true,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
                               Container(
-                                padding: EdgeInsets.only(top: 5),
-                                child: ChannelLogo(
-                                  channelId: id,
+                                child: ProviderLogo(
+                                  size: 15,
+                                  providerId: item.provider,
                                 ),
                               ),
                               Container(
-                                padding: EdgeInsets.only(top: 7),
+                                margin: const EdgeInsets.only(left: 5),
                                 child: Text(
-                                  item.name ?? id,
-                                  softWrap: true,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(top: 5),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Container(
-                                      child: ProviderLogo(
-                                        size: 15,
-                                        providerId: item.provider,
-                                      ),
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.only(left: 5),
-                                      child: Text(
-                                        item.provider.toString().capitalize(),
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                          color: Theme.of(context)
-                                              .textTheme
-                                              .title
-                                              .color
-                                              .withOpacity(0.6),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(top: 2),
-                                child: Container(
-                                  margin: const EdgeInsets.only(left: 5),
-                                  child: Text(
-                                    topics
-                                        .where(
-                                            (it) => item.topics.contains(it.id))
-                                        .map((it) => it.caption)
-                                        .join(','),
-                                    softWrap: true,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Theme.of(context)
-                                          .textTheme
-                                          .title
-                                          .color
-                                          .withOpacity(0.6),
-                                    ),
+                                  item.provider.toString().capitalize(),
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .title
+                                        .color
+                                        .withOpacity(0.6),
                                   ),
                                 ),
                               ),
                             ],
                           ),
                         ),
-                      ),
-                    );
-                  },
+                        StoreConnector<AppState, List<Topic>>(
+                          converter: (Store<AppState> store) => store.state.topics,
+                          builder: (BuildContext context, List<Topic> topics) {
+                            return Container(
+                              padding: const EdgeInsets.only(top: 2),
+                              child: Container(
+                                margin: const EdgeInsets.only(left: 5),
+                                child: Text(
+                                  topics
+                                      .where(
+                                          (it) => item.topics.contains(it.id))
+                                      .map((it) => it.caption)
+                                      .join(','),
+                                  softWrap: true,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .title
+                                        .color
+                                        .withOpacity(0.6),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               );
             },
-          ),
-        ],
+          );
+        },
       ),
     );
   }
