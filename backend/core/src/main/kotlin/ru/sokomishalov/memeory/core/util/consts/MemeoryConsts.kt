6148f0024f8417d8981595d1d8d95f600e1d3b3c
sokomishalov <vtb218497@corp.dev.vtb>
2020-01-17 17:48:26 +0300

    removals

--- backend/core/src/main/kotlin/ru/sokomishalov/memeory/core/util/consts/MemeoryConsts.kt
+++ backend/core/src/main/kotlin/ru/sokomishalov/memeory/core/util/consts/MemeoryConsts.kt
@@ -2,21 +2,7 @@
 
 package ru.sokomishalov.memeory.core.util.consts
 
-// Strings
-const val DELIMITER = ":"
-const val MONGO_ID_FIELD = "_id"
-const val MONGO_KEY_DOT_REPLACEMENT = "_"
-const val MEMEORY_TOKEN_HEADER = "MEMEORY_TOKEN"
-
-const val FACEBOOK_BASE_URL = "https://www.facebook.com"
-const val FACEBOOK_GRAPH_BASE_URL = "http://graph.facebook.com"
-const val REDDIT_BASE_URL = "https://www.reddit.com"
-const val TWITTER_URL = "https://twitter.com"
-const val NINEGAG_URL = "https://9gag.com"
-const val PINTEREST_URL = "https://www.pinterest.com"
-const val VK_URL = "https://vk.com"
-const val IFUNNY_URL = "https://ifunny.co"
-const val INSTAGRAM_URL = "https://www.instagram.com"
-
-// Keys
-const val CHANNEL_LOGO_CACHE_KEY = "CHANNEL_LOGO"
+const val DELIMITER: String = ":"
+const val MONGO_ID_FIELD: String = "_id"
+const val MONGO_KEY_DOT_REPLACEMENT: String = "_"
+const val CHANNEL_LOGO_CACHE_KEY: String = "CHANNEL_LOGO"
